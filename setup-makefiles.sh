73fe5dbd4e1f0aef0f16e256a72189892b5746be
daredevil-07 <43841542+daredevil-07@users.noreply.github.com>
2020-08-22 15:20:57 +0530

    Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -13,9 +13,9 @@ INITIAL_COPYRIGHT_YEAR=2019
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-PA_ROOT="${MY_DIR}/../../.."
+CYGNUS_ROOT="${MY_DIR}/../../.."
 
-HELPER="${PA_ROOT}/vendor/pa/build/tools/extract_utils.sh"
+HELPER="${CYGNUS_ROOT}/vendor/cygnus/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -23,7 +23,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${PA_ROOT}" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${CYGNUS_ROOT}" true
 
 # Copyright headers and guards
 write_headers "davinci phoenix violet"
@@ -37,7 +37,7 @@ write_footers
 if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${PA_ROOT}" false
+    setup_vendor "${DEVICE}" "${VENDOR}" "${CYGNUS_ROOT}" false
 
     # Copyright headers and guards
     write_headers
